6d0400f714da0a32df7f303c33afe4109051b48e
Igor Françozo <igorfrancozo9@gmail.com>
2022-09-29 15:05:52 -0400

    Atualização Tipo - Add_Remove

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
